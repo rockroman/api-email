af872f48eecaeb8378ac2a2f4b0798081973de1e
rockroman <rock.rakic@gmail.com>
2024-03-06 16:08:50 +0000

    overwriting allauth templates2

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -162,7 +162,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'staticfiles', 'build'),
-            os.path.join(BASE_DIR, 'allauth')
+            os.path.join(BASE_DIR, 'templates', 'allauth')
             ],
         'APP_DIRS': True,
         'OPTIONS': {
